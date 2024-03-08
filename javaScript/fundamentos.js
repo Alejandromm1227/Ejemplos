c1cff4badfd91af1b8d2d91e6e93f333cd42c2fb
Alejandromm1227 <160982660+Alejandromm1227@users.noreply.github.com>
2024-03-07 19:50:00 -0500

    javascript

---
+++ javaScript/fundamentos.js
@@ -0,0 +1,21 @@
+// console.log("Mensaje en consola")
+// document.write("mensaje en interfaz de usuario")
+// alert("mensaje en ventana emergente")
+// saludo1 = "Hola!!!"
+// var saludo2 = "Hola 2"
+// let saludo3 = "Hola 3"
+// const saludo4 = "Hola 4"
+// console.log(saludo)
+
+// let numero1 = 1
+// const numero2 = 2
+
+/* numero1 = "1"
+// numero2 = "2"
+console.log(numero1)
+console.log(numero2) */
+
+"use strict"
+
+let numero1 = 1
+console.log(numero1)
